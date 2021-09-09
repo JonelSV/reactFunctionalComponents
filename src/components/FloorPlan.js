9d54697e0be33a5dda4e562e871151b23fbb4f82
JonelSV <svjonel@gmail.com>
2021-09-08 20:42:54 -0400

    'fixed containers'

--- src/components/FloorPlan.js
+++ src/components/FloorPlan.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import BedRoom1 from './BedRoom1'
+import BedRoom2 from './BedRoom2'
+import BedRoom3 from './BedRoom3'
+import Bath1 from './Bath1';
+import Bath2 from './Bath2';
+import LivingRoom from './LivingRoom';
+import Kitchen from './Kitchen';
 
 
 const FloorPlan = () => {
     
     return (
-      <div>
-       
+      <div className='container'>
+          <BedRoom1 />
+          <BedRoom2 />
+          <BedRoom3 />
+          <Bath1 />
+          <Bath2 />
+          <LivingRoom />
+          <Kitchen />
   
   
       </div>
