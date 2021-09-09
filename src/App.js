8d2bfe490b22e3effae993bfad9a309be53b35a1
JonelSV <svjonel@gmail.com>
2021-09-08 20:43:13 -0400

    'fixed'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react' 
 import './App.css'
 import FloorPlan from './components/FloorPlan';
-import BedRoom1 from './components/BedRoom1'
-import BedRoom2 from './components/BedRoom2'
-import BedRoom3 from './components/BedRoom3'
-import Bath1 from './components/Bath1';
-import Bath2 from './components/Bath2';
-import LivingRoom from './components/LivingRoom';
-import Kitchen from './components/Kitchen';
+
 
 
 
@@ -18,16 +12,10 @@ const App = () => {
 
   return (
   
-    <div className='container'>
+    <div>
 
           <FloorPlan />
-          <BedRoom1 />
-          <BedRoom2 />
-          <BedRoom3 />
-          <Bath1 />
-          <Bath2 />
-          <LivingRoom />
-          <Kitchen />
+          
     
 
 
--- src/components/LivingRoom.js
+++ src/components/LivingRoom.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 
+
 const LivingRoom = () => {
 
     return (
       <div className='Living-Room'>
   
-            <h2>LivingRoom</h2>
+          <h2>Living Room</h2>
   
       </div>
     )
